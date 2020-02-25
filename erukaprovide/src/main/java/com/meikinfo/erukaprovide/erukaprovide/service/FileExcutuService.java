0bb3df2c1b8451b4581c918840eaddb59070b79c
hjw <1353208500@qq.com>
2020-02-25 17:35:44 +0800

    多线程事务不安全处理

--- erukaprovide/src/main/java/com/meikinfo/erukaprovide/erukaprovide/service/FileExcutuService.java
+++ erukaprovide/src/main/java/com/meikinfo/erukaprovide/erukaprovide/service/FileExcutuService.java
@@ -1,7 +1,6 @@
 package com.meikinfo.erukaprovide.erukaprovide.service;
 
 import cn.hutool.core.date.DateTime;
-import cn.hutool.core.date.DateUnit;
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
 import com.meikinfo.erukaprovide.erukaprovide.dao.PersonMapper;
@@ -13,11 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-import sun.rmi.runtime.Log;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -42,7 +41,7 @@ public class FileExcutuService {
     public void analysis(MultipartFile file) throws InterruptedException, IOException {
         log.info("【开始导入人员信息数据】起始时间:{}", DateTime.now().toMsStr());
 
-
+            List<Person> peoples=new ArrayList<>();
             ExcelReader reader = ExcelUtil.getReader(file.getInputStream());
             Sheet sheet = reader.getSheet();
             int readtime = (sheet.getLastRowNum() / oneReadNum)+1;
@@ -58,25 +57,31 @@ public class FileExcutuService {
                     end =(i+1)*oneReadNum;
                 }
                 executorService.execute(()->{
-                    batchInsert2Db(sheet,start,end,latch);
+                    List<Person> list = batchBuildData(sheet, start, end, latch);
+                    peoples.addAll(list);
                 });
             });
             latch.await();
             executorService.shutdown();
 
+            Optional.of("人员总数"+peoples.size()+"=========").ifPresent(System.out::println);
+
+            personMapper.insertList(peoples);
+
         log.info("【导入人员信息数据完成】完成时间:{}", DateTime.now().toMsStr());
 
     }
 
-    public void batchInsert2Db(Sheet sheet, int start, int end,CountDownLatch latch) {
+    public List<Person> batchBuildData(Sheet sheet, int start, int end, CountDownLatch latch) {
         List<Person> list = new ArrayList<>();
         IntStream.range(start,end).boxed().forEach(i->{
             Row row = sheet.getRow(i);
             Person person = new Person(row.getCell(0).getStringCellValue(), row.getCell(1).getStringCellValue(), String.valueOf(row.getCell(2).getNumericCellValue()));
             list.add(person);
         });
-        int insertnum = personMapper.insertList(list);
-        log.info("【导入人员信息数据成功】起始行号：{} 完成行号{}", start,end);
+        log.info("【线程:{}完成人员信息组装:】 起始行数：{} 结束行数{}", Thread.currentThread().getName(),start,end);
         latch.countDown();
+        return list;
+
     }
 }
